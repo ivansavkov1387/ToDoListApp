22a2de9b339a46f99eaf49339bedafd6997dbfa4
Ivan Savkov <ivansavkov1387@gmail.com>
2020-10-03 14:31:34 +0300

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Иван on 10/3/20.
+//  Copyright © 2020 Ivan Savkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
